206138622181d0c265c38290023544ccee5d7ea3
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2023-03-08 12:59:06 -0300

    Add ESLint

--- src/pages/error-page.tsx
+++ src/pages/error-page.tsx
@@ -1,6 +1,7 @@
 import { Link, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const error: any = useRouteError();
 
   return (
